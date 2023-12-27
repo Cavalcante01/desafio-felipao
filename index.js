e1988449a72514a99d55a63ad979563d8d0d4a1b
Cavalcante01 <136639758+Cavalcante01@users.noreply.github.com>
2023-12-27 11:29:26 -0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 cavalcante
+function calcularRank(vitorias, derrotas) {
+    let saldoVitorias = vitorias - derrotas;
+    let nivel;
+
+    if (vitorias < 10) {
+        nivel = "Ferro";
+    } else if (vitorias <= 20) {
+        nivel = "Bronze";
+    } else if (vitorias <= 50) {
+        nivel = "Prata";
+    } else if (vitorias <= 80) {
+        nivel = "Ouro";
+    } else if (vitorias <= 90) {
+        nivel = "Diamante";
+    } else if (vitorias <= 100) {
+        nivel = "Lendário";
+    } else {
+        nivel = "Imortal";
+    }
+  
+    console.log(`O Herói tem um saldo de ${saldoVitorias} e está no nível de ${nivel}`);
+}
+
+calcularRank(1000, 0);
